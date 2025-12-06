4e3c93035d959ea36ec89d6581c49a7389d6cdef
Cameron Reed <cameron@cam123.dev>
2025-12-05 21:21:45 -0700

    Update base to include allocator

--- base/src/main.zig
+++ base/src/main.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 
 
 pub fn main() !void {
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    const allocator = gpa.allocator();
+    defer {
+        if (gpa.deinit() == .leak) {
+            std.debug.print("Memory was leaked! D:\n", .{});
+        }
+    }
+
     var stdin_buf: [1024]u8 = undefined;
     var stdout_buf: [1024]u8 = undefined;
 
@@ -21,22 +29,24 @@ pub fn main() !void {
 
     var result: u64 = 0;
     if (std.mem.eql(u8, std.mem.span(std.os.argv[1]), "--part2")) {
-        result = try part2(stdin, stdout);
+        result = try part2(allocator, stdin);
     } else {
-        result = try part1(stdin, stdout);
+        result = try part1(allocator, stdin);
     }
 
     try stdout.print("Result: {}\n", .{ result });
 }
 
 
-fn part1(stdin: *std.Io.Reader, _: *std.Io.Writer) !u64{
+fn part1(allocator: std.mem.Allocator, stdin: *std.Io.Reader) !u64{
+    _ = allocator;
     _ = stdin;
     return 0;
 }
 
 
-fn part2(stdin: *std.Io.Reader, _: *std.Io.Writer) !u64 {
+fn part2(allocator: std.mem.Allocator, stdin: *std.Io.Reader) !u64 {
+    _ = allocator;
     _ = stdin;
     return 0;
 }
